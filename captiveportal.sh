97a8bcec102d4ade26cbb84833992337b989998d
dinnerisserved <dinnerisserved@users.noreply.github.com>
2023-10-28 16:06:12 +0100

    Update captiveportal.sh
    All errors known should have been fixed with this update

--- captiveportal.sh
+++ captiveportal.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-# Get current SSID 
-ssid=$(nmcli -fields NAME,SSID connection show --active | egrep '^[a-zA-Z0-9-_]+' -o)
+# Get current SSID
+ssid=$(nmcli -t -f active,ssid dev wifi | grep '^yes:' | cut -d: -f2)
+
+# Check if SSID is empty
+if [ -z "$ssid" ]; then
+    echo "No active Wi-Fi connection found."
+    exit 1
+fi
 
 # Disconnect and reconnect
 nmcli con down id "$ssid"
-nmcli con up id "$ssid" 
+nmcli con up id "$ssid"
 
 # Restart NetworkManager
 sudo service NetworkManager restart
 
-# Flush DNS cache  
-sudo /etc/init.d/dns-clean start
+# Flush DNS cache
+sudo systemctl restart systemd-resolved
 
 # Disable firewall temporarily
-sudo ufw disable 
+sudo ufw disable
 
-# Force captive portal to open
+# Adjust proxy settings and force captive portal to open
 gsettings set org.gnome.system.proxy mode 'none'
-gsettings set org.gnome.system.proxy autoconfig-url "http://example.com/captive"
+gsettings set org.gnome.system.proxy autoconfig-url ""
 
-# Open captive portal page 
-xdg-open http://example.com  
+# Try opening an HTTP and an HTTPS site to trigger the captive portal
+xdg-open http://example.com
+xdg-open https://example.com
 
 # Revert proxy settings
 gsettings set org.gnome.system.proxy mode 'auto'
 gsettings reset org.gnome.system.proxy autoconfig-url
 
-# Trigger web request
-xdg-open http://example.com 
+# Trigger additional web requests
+xdg-open http://neverssl.com
+xdg-open https://google.com
 
 # Re-enable firewall
 sudo ufw enable
